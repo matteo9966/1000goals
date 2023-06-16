633420a8b8a768fdf1c7fb42d3cc3ea01bf117f8
matteo9966 <matteo_tommasi96@hotmail.com>
2023-06-16 07:54:54 +0200

    added card component

---
+++ src/app/components/goal-card/goal-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-goal-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './goal-card.component.html',
+  styleUrls: ['./goal-card.component.scss']
+})
+export class GoalCardComponent {
+
+}
