20b2da54dd9999782f3e863d52740274d2d7519b
matteo9966 <matteo_tommasi96@hotmail.com>
2023-06-16 09:08:25 +0200

    added goal cards

--- src/app/pages/goals/goals.component.ts
+++ src/app/pages/goals/goals.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GoalCardComponent } from 'src/app/components/goal-card/goal-card.component';
 
 @Component({
   selector: 'app-goals',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,GoalCardComponent],
   templateUrl: './goals.component.html',
   styleUrls: ['./goals.component.scss']
 })
--- src/app/pages/proposed-goals/proposed-goals.component.ts
+++ src/app/pages/proposed-goals/proposed-goals.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GoalCardComponent } from 'src/app/components/goal-card/goal-card.component';
 
 @Component({
   selector: 'app-proposed-goals',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,GoalCardComponent],
   templateUrl: './proposed-goals.component.html',
   styleUrls: ['./proposed-goals.component.scss']
 })
 export class ProposedGoalsComponent {
-
+ //TODO: add a like badge or dislike badge like accepted or refused 
 }
