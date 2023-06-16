ef322a1414f79c2af1133fea319a1b61c0485285
matteo9966 <matteo_tommasi96@hotmail.com>
2023-06-16 15:33:30 +0200

    added proposed goals form section

---
+++ src/app/components/goal-form/goal-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-goal-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './goal-form.component.html',
+  styleUrls: ['./goal-form.component.scss']
+})
+export class GoalFormComponent {
+
+}
--- src/app/pages/proposed-goals/proposed-goals.component.ts
+++ src/app/pages/proposed-goals/proposed-goals.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoalCardComponent } from 'src/app/components/goal-card/goal-card.component';
+import { GoalFormComponent } from 'src/app/components/goal-form/goal-form.component';
 
 @Component({
   selector: 'app-proposed-goals',
   standalone: true,
-  imports: [CommonModule,GoalCardComponent],
+  imports: [CommonModule, GoalCardComponent, GoalFormComponent],
   templateUrl: './proposed-goals.component.html',
-  styleUrls: ['./proposed-goals.component.scss']
+  styleUrls: ['./proposed-goals.component.scss'],
 })
-export class ProposedGoalsComponent {
- //TODO: add a like badge or dislike badge like accepted or refused 
+export class ProposedGoalsComponent implements OnInit {
+
+  private _selected:'proposed'|'form'='proposed';
+
+  @ViewChild('proposedgoals', { read: TemplateRef, static: true })
+  proposedGoalsTemplate!: TemplateRef<any>;
+
+  @ViewChild('form', { read: TemplateRef, static: true })
+  formTemplate!: TemplateRef<any>;
+
+  showedTemplate:TemplateRef<any>|null=null;
+
+  ngOnInit(): void {
+   this.selected='proposed';
+  }
+
+  onClickMenu(type:"form"|"proposed"){
+    this.selected=type;
+  }
+
+  set selected(value:'proposed'|'form'){
+    this._selected=value;
+    if(value==='form'){
+      this.showedTemplate=this.formTemplate
+      return
+    }
+    if(value==='proposed'){
+      this.showedTemplate=this.proposedGoalsTemplate
+    }
+  }
+
+  get selected(){
+    return this._selected;
+  }
+
+  
 }
